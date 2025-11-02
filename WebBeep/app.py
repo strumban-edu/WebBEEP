2fa422700c269178a712c2ce263c7e17b11a4c47
OwenKlochack <Oklochack@oakland.edu>
2025-11-02 18:50:56 -0500

    Display Events
    Events are now displayed from the database and you can add new ones.

--- WebBeep/app.py
+++ WebBeep/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('dotenv.env')
 
 app = Flask(__name__)
 
@@ -18,7 +18,50 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    return render_template('WebBeepMockup.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    cur.execute('''
+        SELECT e.eventid, e.eventname, e.category, e.status, e.eventtime, l.locationname
+        FROM "Event" e
+        JOIN "Location" l ON e.locationid = l.locationid
+        ORDER BY e.eventid;
+    ''')
+    
+    events = cur.fetchall()
+    cur.close()
+    conn.close()
+    
+    return render_template('WebBeepMockup.html', events=events)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/add-event', methods=['GET', 'POST'])
+def add_event():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    # Fetch locations for dropdown
+    cur.execute('SELECT locationid, locationname FROM "Location" ORDER BY locationname;')
+    locations = cur.fetchall()
+    
+    if request.method == 'POST':
+        eventname = request.form['eventname']
+        category = request.form['category']
+        status = request.form['status']
+        eventtime = request.form['eventtime']
+        locationid = request.form['locationid']
+        creatorid = 1  # For now, default user id; later can tie to logged-in user
+        
+        cur.execute('''
+            INSERT INTO "Event" (eventname, category, status, eventtime, locationid, creatorid)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        ''', (eventname, category, status, eventtime, locationid, creatorid))
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+    
+    cur.close()
+    conn.close()
+    return render_template('add_event.html', locations=locations)
