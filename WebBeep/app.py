25b1d2a1f9372459558ae8db4c123f38cb39b843
kraftw <kraftworker12@gmail.com>
2025-11-02 12:13:57 -0500

    Added Database Connection

--- WebBeep/app.py
+++ WebBeep/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    return conn
 
 @app.route('/')
 def index():
